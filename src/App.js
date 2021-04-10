de8aa547de840bfdc211496f7bb711d8512e5dff
Zeeshan Safdar <zeeshan7826@gmail.com>
2021-04-10 15:17:36 +0500

    add react router for navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => {
+  return <h1>Hats Page</h1>;
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-export default function MenuItem({ title, imageUrl, size }) {
+const MenuItem = ({ title, imageUrl, size, match, history, linkUrl }) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
       <div
         style={{
           backgroundImage: `url(${imageUrl})`,
@@ -17,4 +18,6 @@ export default function MenuItem({ title, imageUrl, size }) {
       </div>
     </div>
   );
-}
+};
+
+export default withRouter(MenuItem);

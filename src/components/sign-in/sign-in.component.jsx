9cb0f76e9a6366bb2d2b93c878f35c276e5a1b9e
Zeeshan Safdar <zeeshan.safdar@systemsltd.com>
2021-04-22 11:53:11 +0500

    add sign in and sign up with firebase

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -9,10 +10,15 @@ const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const onSubmitForm = (event) => {
+  const onSubmitForm = async (event) => {
     event.preventDefault();
-    setEmail('');
-    setPassword('');
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      setEmail('');
+      setPassword('');
+    } catch (e) {
+      console.log('Error:', e);
+    }
   };
 
   const handleChangeInput = (event) => {

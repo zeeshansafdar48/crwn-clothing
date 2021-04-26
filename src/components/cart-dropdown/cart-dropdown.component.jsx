a85abc53af19c7a0ee42a89cdd21f35bd6dd2afd
Zeeshan Safdar <zeeshan.safdar@systemsltd.com>
2021-04-26 14:50:10 +0500

    add cart item view

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,13 @@
+import { useSelector } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
+
 import './cart-dropdown.styles.scss';
 
 export default function CartDropdown() {
+  const data = useSelector();
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
+      <div className="cart-items"></div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );

a74f2b5e9cde778bf0b129f59e292112df44501f
Zeeshan Safdar <zeeshan.safdar@systemsltd.com>
2021-04-27 14:51:09 +0500

    move collection data to redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import SHOP_DATA from './shop.data';
 
 function ShopPage() {
-  const [collections] = useState(SHOP_DATA);
+  const collections = useSelector((state) => state.collections?.collections);
   return (
     <div className="shop-page">
       {collections.map(({ id, ...otherCollectionsProps }) => (
--- src/redux/reducers/root-reducer.js
+++ src/redux/reducers/root-reducer.js
@@ -5,11 +5,13 @@ import { persistReducer } from 'redux-persist';
 import UserReducer from './user/user.reducer';
 import CartReducer from './cart/cart.reducer';
 import DirectoryReducer from './directory/directory.reducer';
+import CollectionReducer from './collection/collection.reducer';
 
 const rootReducer = combineReducers({
   user: UserReducer,
   cart: CartReducer,
   directory: DirectoryReducer,
+  collections: CollectionReducer,
 });
 
 const persistConfig = {

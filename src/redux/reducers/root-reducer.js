75ba3b7659698a85026be0cab20cdbec39bcdf37
Zeeshan Safdar <zeeshan.safdar@systemsltd.com>
2021-04-27 11:23:58 +0500

    add redux-persist

--- src/redux/reducers/root-reducer.js
+++ src/redux/reducers/root-reducer.js
@@ -1,8 +1,19 @@
 import { combineReducers } from 'redux';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+
 import UserReducer from './user/user.reducer';
 import CartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const rootReducer = combineReducers({
   user: UserReducer,
   cart: CartReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+export default persistReducer(persistConfig, rootReducer);

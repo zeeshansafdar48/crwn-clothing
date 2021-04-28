1730bd2df873f47dae5d7138d0b6310bf7378c6c
Zeeshan Safdar <zeeshan.safdar@systemsltd.com>
2021-04-28 16:17:53 +0500

    convert array to object for fast query

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,22 +2,31 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCollectionByRouteName } from '../../redux/reducers/collection/collection.action';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './collection.styles.scss';
 
 function CollectionPage() {
   const params = useParams();
   const dispatch = useDispatch();
   const selectedCollections = useSelector((state) => state?.collections?.selectedCollections);
-  console.log('params', params);
-  console.log('selectedCollections', selectedCollections);
 
   useEffect(() => {
     if (params) {
       dispatch(getCollectionByRouteName(params?.collectionId));
     }
   }, [params, dispatch]);
+
+  const { title, items } = selectedCollections || {};
+
   return (
-    <div className="shop-page">
-      <h1>Collection Page</h1>
+    <div className="collection-page">
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items?.map((item) => (
+          <CollectionItem key={item?.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 }
